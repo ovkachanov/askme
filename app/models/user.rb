2c19d5bd824712dad788a6166cf03c1791bfe866
ovkachanov <ovkachanov1988@gmail.com>
2018-04-09 17:19:06 +0300

    add user, qustion, validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,42 @@
+require 'openssl'
+
 class User < ApplicationRecord
+
+  INTEGRATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :question
+
+  validates :email, :name, presence: true
+  validates :email, :name, uniqueness: true
+
+  attr_accessor :password
+
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, INTEGRATIONS, DIGEST.length, DIGEST)
+      )
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, INTEGRATIONS, DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
 end
+
