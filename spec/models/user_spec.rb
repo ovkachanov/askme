9ffcc0c28f7e326064961dc5353300757ed6f6bb
ovkachanov <ovkachanov1988@gmail.com>
2018-04-14 17:41:55 +0300

    add unique username index in  db

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 describe User do
-  let(:user) { build(:user) }
+  let!(:user) { create(:user) }
 
   it { should validate_presence_of :name }
-  it { should validate_presence_of :email }
   it { should validate_uniqueness_of :email }
-  it { should validate_uniqueness_of :username }
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of(:username).ignoring_case_sensitivity }
   it { should have_many(:questions) }
 
   it "is valid with valid attributes" do
