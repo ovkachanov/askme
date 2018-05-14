fd9a6a104ce7730b814edc2988a76ca0d8b63322
ovkachanov <ovkachanov1988@gmail.com>
2018-05-14 17:12:39 +0300

    add before_update callback & fix dublicate code

--- app/helpers/hashtags_helper.rb
+++ app/helpers/hashtags_helper.rb
@@ -1,10 +1,5 @@
 module HashtagsHelper
-  def render_hashtag_link_question(text)
-    text.gsub(/#[[:alpha:]]+/){ |word| link_to word, "/hashtags/#{word.downcase.delete('#')}"}.html_safe
-  end
-
-
-  def render_hashtag_link_answer(answer)
-    answer.gsub(/#[[:alpha:]]+/){ |word| link_to word, "/hashtags/#{word.downcase.delete('#')}"}.html_safe
+  def render_hashtag_link(obj)
+    obj.gsub(/#[[:alpha:]]+/){ |word| link_to word, "/hashtags/#{word.downcase.delete('#')}"}.html_safe
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -8,11 +8,23 @@ class Question < ApplicationRecord
   validates :text, length: { maximum: 255 }
 
   after_create :create_hashtag
+  before_update :update_hashtag
 
   private
 
   def create_hashtag
     question = Question.find_by(id: self.id)
+    tags = self.text.scan(/#[[:alpha:]]+/)
+    tags << self.answer.scan(/#[[:alpha:]]+/) if self.answer.present?
+    tags.uniq.map do |hashtag|
+      hashtag = Hashtag.find_or_create_by(name: hashtag.to_s.downcase.delete('#'))
+      question.hashtags << hashtag
+    end
+  end
+
+  def update_hashtag
+    question = Question.find_by(id: self.id)
+    self.hashtags.clear
     tags = self.text.scan(/#[[:alpha:]]+/)
     tags << self.answer.scan(/#[[:alpha:]]+/) if self.answer.present?
     tags.uniq.map do |hashtag|
