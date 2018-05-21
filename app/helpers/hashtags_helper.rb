0881435315279ab72268bf2065c37ed680856ab5
ovkachanov <ovkachanov1988@gmail.com>
2018-05-21 14:20:42 +0300

    correct: hashtags are not displayed without related questions

--- app/helpers/hashtags_helper.rb
+++ app/helpers/hashtags_helper.rb
@@ -1,7 +1,7 @@
 module HashtagsHelper
   def render_hashtag_link(obj)
     if hashtag_name_valid?(obj)
-      obj.gsub(/#[[:alpha:]]+/){ |word| link_to word, "/hashtags/#{word.downcase.delete('#')}"}.html_safe
+      obj.gsub(/#[\p{L}0-9_]{1,35}/){ |word| link_to word, "/hashtags/#{word.downcase.delete('#')}"}.html_safe
     else
       obj
     end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,19 +2,24 @@ class Question < ApplicationRecord
 
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
-  has_and_belongs_to_many :hashtags
+  has_and_belongs_to_many :hashtags, dependent: :destroy
 
   validates :text, :user, presence: true
   validates :text, length: { maximum: 255 }
 
   before_save :add_hashtag
 
+
   private
+
+  def questions_with_hashtag
+    self.includes(:hashtags)
+  end
 
   def add_hashtag
     self.hashtags.clear
-    tags = self.text.scan(/#[[:alpha:]]+/)
-    tags << self.answer.scan(/#[[:alpha:]]+/) if self.answer.present?
+    tags = self.text.scan(/#[\p{L}0-9_]{1,35}/)
+    tags << self.answer.scan(/#[\p{L}0-9_]{1,35}/) if self.answer.present?
     tags.uniq.map do |hashtag|
       hashtag = Hashtag.find_or_create_by(name: hashtag.to_s.downcase.delete('#[]"'))
       self.hashtags << hashtag unless hashtag.nil?
